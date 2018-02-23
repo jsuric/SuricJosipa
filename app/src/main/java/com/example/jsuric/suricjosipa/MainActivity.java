4b6a16bc79ea188b1e08c254951d61deaffc1531
jsuric <josipa.suric@gmail.com>
2018-02-23 19:46:33 +0100

    Second commit

--- app/src/main/java/com/example/jsuric/suricjosipa/MainActivity.java
+++ app/src/main/java/com/example/jsuric/suricjosipa/MainActivity.java
@@ -1,7 +1,13 @@
 package com.example.jsuric.suricjosipa;
 
 import android.app.ListActivity;
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Intent;
 import android.database.Cursor;
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -13,10 +19,13 @@ import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+    int notificationID=1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        displayNotification();
 
         DBAdapter db = new DBAdapter(this);
 
@@ -162,5 +171,75 @@ public class MainActivity extends AppCompatActivity {
                 "Film: " + c.getString(0) + "\n" +
                         "Id: " + c.getString(1),
                 Toast.LENGTH_LONG).show(); */
+    }
+
+    protected void displayNotification()
+    {
+        //---PendingIntent to launch activity if the user selects
+        // this notification---
+        Intent i = new Intent(this, NotificationView.class);
+
+        i.putExtra("notificationID", notificationID);
+
+
+        PendingIntent pendingIntent =
+                PendingIntent.getActivity(this, 0, i, 0);
+
+        long[] vibrate = new long[] { 100, 250, 100, 500};
+
+//Notification Channel - novo od Android O
+
+        String NOTIFICATION_CHANNEL_ID = "my_channel_01";
+        CharSequence channelName = "hr.math.karga.MYNOTIF";
+        int importance = NotificationManager.IMPORTANCE_LOW;
+        NotificationChannel notificationChannel = new NotificationChannel(NOTIFICATION_CHANNEL_ID, channelName, importance);
+        notificationChannel.enableLights(true);
+        notificationChannel.setLightColor(Color.RED);
+        notificationChannel.enableVibration(true);
+        notificationChannel.setVibrationPattern(vibrate);
+
+//za sve verzije
+        NotificationManager nm = (NotificationManager)
+                getSystemService(NOTIFICATION_SERVICE);
+
+// za Notification Chanel
+
+        nm.createNotificationChannel(notificationChannel);
+
+
+
+
+//ovako je i u starim verzijama, jedino dodano .setChannelId (za stare verzije to brisemo)
+
+        Notification notif = new Notification.Builder(this)
+                .setTicker("Reminder: meeting starts in 5 minutes")
+                .setContentTitle("Meeting with customer at 3pm...")
+                .setContentText("this is the second row")
+                .setSmallIcon(R.mipmap.ic_launcher)
+                .setWhen(System.currentTimeMillis())
+                .setShowWhen(true)
+                .setContentIntent(pendingIntent)
+                .setVibrate(vibrate)
+                .setChannelId(NOTIFICATION_CHANNEL_ID)
+                .build();
+        //najnovije, od API level 26.1.0., .setWhen ide po defautlu ovdje na currentTimeMillis
+
+/*        final NotificationCompat.Builder notif = new NotificationCompat.Builder(this,NOTIFICATION_CHANNEL_ID)
+
+                .setDefaults(Notification.DEFAULT_ALL)
+                .setSmallIcon(R.mipmap.ic_launcher)
+                .setVibrate(vibrate)
+                .setSound(null)
+                .setChannelId(NOTIFICATION_CHANNEL_ID)
+                .setContentTitle("Meeting with customer at 3pm...")
+                .setContentText("this is the second row")
+                .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+                .setTicker("Reminder: meeting starts in 5 minutes")
+                .setContentIntent(pendingIntent)
+                .setAutoCancel(false); */
+
+// za sve verzije
+
+        nm.notify(notificationID, notif);
     }
 }
