a20c8ae7b8c1d4cded92a5638ad57ee94fe816ae
jsuric <josipa.suric@gmail.com>
2018-02-23 20:19:35 +0100

    One more time

---
+++ app/src/main/java/com/example/jsuric/suricjosipa/NotificationView.java
@@ -0,0 +1,19 @@
+package com.example.jsuric.suricjosipa;
+
+import android.app.NotificationManager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class NotificationView extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.notification);
+        //dohvat Notification Managera
+        NotificationManager nm=(NotificationManager)getSystemService(NOTIFICATION_SERVICE);
+        //cancel metoda za gasenje notificationa na kraju
+        //proba
+        nm.cancel(getIntent().getExtras().getInt("notificationID"));
+    }
+}
